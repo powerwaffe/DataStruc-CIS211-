146237948500f482b0a73cb844b0f7d5fe7e6087
Sean <powerwaffe@gmail.com>
2016-09-05 21:26:44 -0400

    Modified ArraySet and TestArraySet

--- src/Projects/ArraySet2.java
+++ src/Projects/ArraySet2.java
@@ -2,26 +2,27 @@ package Projects;
 /**
  * Created by Sean on 8/31/2016.
  */
-public final class ArraySet<T> implements SetInterface<T>
+public final class ArraySet2<T> implements SetInterface<T>
 {
     private T[] bag; // Cannot be final due to doubling
     private int numberOfEntries;
     private boolean initialized = false;
     private static final int DEFAULT_CAPACITY = 25; // Initial capacity of bag
     private static final int MAX_CAPACITY = 10000;
+    private final int NOT_FOUND = -1;
     public ResizableArrayBag resizableArrayBag = new ResizableArrayBag(); //create object to be called
 
-    public ArraySet(ResizableArrayBag ResizeBag)
+    public ArraySet2(ResizableArrayBag ResizeBag)
     {
         this.resizableArrayBag = resizableArrayBag;
     } //end constructor
 
-    public ArraySet()
+    public ArraySet2()
     {
         this(DEFAULT_CAPACITY);
     } //end default constructor
 
-    public ArraySet(int initialCapacity)
+    public ArraySet2(int initialCapacity)
     {
         T[] tempBag = (T[]) new Object[initialCapacity];
         bag = tempBag;
@@ -33,7 +34,7 @@ public final class ArraySet<T> implements SetInterface<T>
     {
         return resizableArrayBag.getCurrentSize();
     }
-     //end getCurrentSize
+    //end getCurrentSize
 
     public boolean isEmpty()
     {
@@ -42,6 +43,18 @@ public final class ArraySet<T> implements SetInterface<T>
 
     public boolean add(T newEntry)
     {
+        /**if(contains(newEntry))
+         return false;
+         bag[numberOfEntries] = newEntry;
+         numberOfEntries++;
+         return true;*/
+
+        /**
+         for(int index = 0; index < numberOfEntries; index++)
+         if(bag[index] == newEntry)
+         return false;
+         return resizableArrayBag.add(newEntry);
+         */
         if(!(contains(newEntry)))
         {
             resizableArrayBag.add(newEntry);
@@ -52,11 +65,44 @@ public final class ArraySet<T> implements SetInterface<T>
 
     public boolean remove(T anEntry)
     {
+        /**(int search = NOT_FOUND;
+         if(isEmpty())
+         {
+
+         }
+         for(int index = 0; index < numberOfEntries && search == NOT_FOUND; index++)
+         {
+         if(bag[index].equals(anEntry))
+         {
+         search = index;
+         }
+         }
+         if(search == NOT_FOUND)
+         {
+         throw NoSuchElementException;
+         }
+         T result = bag[search];
+
+         bag[search] = bag[numberOfEntries - 1];
+         bag[numberOfEntries - 1] = null;
+         numberOfEntries--;
+         resizableArrayBag.remove(anEntry);
+
+         return result;*/
         return resizableArrayBag.remove(anEntry);
     } //end remove
 
     public T remove()
     {
+        /**T result = null;
+         if(numberOfEntries > 0)
+         {
+         result = bag[numberOfEntries - 1];
+         bag[numberOfEntries - 1] = null;
+         numberOfEntries--;
+         }
+         return result;
+         */
         return (T) resizableArrayBag.remove();
     } //end remove
 
