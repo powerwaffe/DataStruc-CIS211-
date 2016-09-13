96e9b705e6c59f5a051627df6f714c48676a56b5
Sean <powerwaffe@gmail.com>
2016-09-12 20:32:05 -0400

    Added package Chapter4Project1 as well as a program.

--- src/Chapter4Project1/AlgorithmEfficiency.java
+++ src/Chapter4Project1/AlgorithmEfficiency.java
@@ -23,7 +23,7 @@ public class AlgorithmEfficiency
             long duration1 = (endTime1 - startTime1); //Calculate time ran
 
             //Print results for algorithm A
-            System.out.println(n + "\t\t\t" + duration1 + "\t\t" + sum);
+            System.out.println(n + "\t\t\t" + duration1 + " ns\t\t" + sum);
             n+=10000; //Increment n by 10,000
         } //end for
 
@@ -35,17 +35,17 @@ public class AlgorithmEfficiency
         n = 10000; //Reset n to 10,000
         for (int k = 0; k < 3; k++) //Run algorithm B 3 times
         {
-            long startTime2 = System.nanoTime();
+            long startTime2 = System.nanoTime(); //Start timer for beginning of algorithm
             for (long i = 1; i <= n; i++)
             {
                 for (long j = 1; j <= i; j++)
                     sum = sum + 1;
             } // end for
-            long endTime2 = System.nanoTime();
+            long endTime2 = System.nanoTime(); //End timer for end of algorithm
             long duration2 = (endTime2 - startTime2);
 
             //Print results for algorithm B
-            System.out.println(n + "\t\t\t" + duration2 + "\t\t" + sum);
+            System.out.println(n + "\t\t\t" + duration2 + " ns\t\t" + sum);
             n+=10000; //Increment n by 10,000
         } //end for
 
@@ -56,13 +56,13 @@ public class AlgorithmEfficiency
         n = 10000;
         for (int i = 0; i < 3; i++)
         {
-            long startTime3 = System.nanoTime();
+            long startTime3 = System.nanoTime(); //Start timer for beginning of algorithm
             sum = n * (n + 1) / 2;
-            long endTime3 = System.nanoTime();
+            long endTime3 = System.nanoTime(); //End timer for end of algorithm
             long duration3 = (endTime3 - startTime3);
 
             //Print results for algorithm C
-            System.out.println(n + "\t\t\t" + duration3 + "\t\t" + sum);
+            System.out.println(n + "\t\t\t" + duration3 + " ns\t\t" + sum);
             n+=10000; //Increment n by 10,000
         } //end for
     } //end main
